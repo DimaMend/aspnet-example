8e9f1c1575f8fcf2923aaf698c2fbaa7769008c9
rubenmeijers <ruben@sebsoft.nl>
2016-06-02 11:23:36 +0200

    betaaldemo
    added : betaaldemo

---
+++ betaalDemo/betaalDemo/exchange.aspx.cs
@@ -0,0 +1,57 @@
+﻿using PAYNLSDK;
+using System;
+using System.Diagnostics;
+
+namespace betaalDemo
+{
+    public partial class exchange : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            PAYNLSDK.API.RequestBase.ApiToken = "0940e0c93212f883616e57496288b76fe6cd850d";
+            PAYNLSDK.API.RequestBase.ServiceId = "SL-1094-0450";
+
+            /*
+
+            */
+            if (String.IsNullOrEmpty(Request.QueryString["order_id"]))
+            {
+                Response.Write("false|unable to process");
+                return;
+            }
+
+
+            PAYNLSDK.API.PaymentMethod.GetAll.Response response = PaymentMethod.GetAll();
+
+            Debug.WriteLine(Request.QueryString);
+            string tranidreader = Request.QueryString["order_id"];
+            if(tranidreader != null)
+            {
+                Response.Write("Param is");
+                Response.Write(tranidreader);
+            }
+          
+            PAYNLSDK.API.Transaction.Info.Response info = Transaction.Info(tranidreader);
+            PAYNLSDK.Enums.PaymentStatus result = info.PaymentDetails.State;
+            Debug.WriteLine(result);
+
+            if (PAYNLSDK.Transaction.IsPaid(result))
+            {
+                // This will only happen when the transaction is paid
+                // TODO: process this in YOUR OWN backend
+                Response.Write("true|Processed");
+                
+            }
+            else if (PAYNLSDK.Transaction.IsCancelled(result))
+            {
+                // Process the cancel
+                Response.Write("true|processed cancel correctly");
+            }
+            else
+            {
+                // Process this in your own backend (
+                Response.Write("true|Processed (probably pending??)");
+            }
+        }
+    }
+}
